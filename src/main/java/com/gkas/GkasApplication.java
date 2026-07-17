49d6bbcc4459a93c7ecf8cd382cbc2fe3c756fbd
Marwill Ranosa <drexxerity@gmail.com>
2026-07-17 17:31:21 +0000

    update

---
+++ src/main/java/com/gkas/GkasApplication.java
@@ -0,0 +1,11 @@
+package com.gkas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GkasApplication.class, args);
+    }
+}
